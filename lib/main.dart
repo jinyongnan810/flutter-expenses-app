eb46c5599d5131d4e10bd6a25e52127007deee27
Yuunan Kin <yuunan.kin@gmail.com>
2021-06-25 08:58:45 +0900

    add a form & submit

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,28 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final List<Transaction> transactions = [
+  List<Transaction> transactions = [
     Transaction('id1', "bag", 11.11, DateTime.now()),
     Transaction('id2', "shoe", 33, DateTime.now()),
     Transaction('id3', "gas", 200, DateTime.now()),
     Transaction('id4', "computer", 10000, DateTime.now()),
   ];
+  String titleInput = '';
+  String amountInput = '';
+  void _onSubmit() {
+    String txId = 'id${this.transactions.length + 1}';
+    setState(() {
+      this.transactions.add(Transaction(
+          txId, titleInput, double.parse(amountInput), DateTime.now()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Personal Expenses',
         theme: ThemeData(
-          primarySwatch: Colors.green,
+          primarySwatch: Colors.purple,
         ),
         home: Scaffold(
           appBar: AppBar(
@@ -38,6 +47,37 @@ class _AppState extends State<App> {
                     child: Text('chart'),
                     elevation: 5,
                   )),
+              Card(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        style: TextStyle(fontSize: 18),
+                        onChanged: (text) => {this.titleInput = text},
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Amount'),
+                        style: TextStyle(fontSize: 18),
+                        onChanged: (text) => {this.amountInput = text},
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        child: OutlinedButton(
+                            onPressed: _onSubmit,
+                            style: ButtonStyle(
+                              padding:
+                                  MaterialStateProperty.all<EdgeInsetsGeometry>(
+                                      EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 15)),
+                            ),
+                            child: Text('Submit')),
+                      )
+                    ],
+                  ),
+                ),
+              ),
               Column(
                 children: [...transactions.map((t) => TransactionListItem(t))],
               )
