c722200da3ffb511ff37e2b9c4fcec90d416dfce
Yuunan Kin <yuunan.kin@gmail.com>
2021-06-29 08:01:23 +0900

    transfer to stateful wgt & add popup form

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import './widgets/user-transactions.dart';
+import './widgets/transaction-form.dart';
+import './widgets/transaction-list.dart';
+import './models/transaction.dart';
 
 void main() {
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
+// MyApp should be seperate with MyHomePage to avoid unexpected error
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,30 +17,67 @@ class App extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Personal Expenses'),
-            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                    width: double.infinity,
-                    child: Card(
-                      child: Text('chart'),
-                      elevation: 5,
-                    )),
-                UserTransactions(),
-              ],
-            ),
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {},
-          ),
-        ));
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // TODO: Work not so well in android
+  void startAddingTransactions(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (BuildContext _) {
+          return TransactionForm(_onSubmit);
+        });
+  }
+
+  List<Transaction> transactions = [
+    Transaction('id1', "bag", 11.11, DateTime.now()),
+    Transaction('id2', "shoe", 33, DateTime.now()),
+    Transaction('id3', "gas", 200, DateTime.now()),
+    Transaction('id4', "computer", 1000, DateTime.now()),
+  ];
+  void _onSubmit(title, amount) {
+    String txId = 'id${this.transactions.length + 1}';
+    setState(() {
+      this.transactions.add(Transaction(txId, title, amount, DateTime.now()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal Expenses'),
+        actions: [
+          IconButton(
+              onPressed: () => startAddingTransactions(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text('chart'),
+                  elevation: 5,
+                )),
+            TransactionList(transactions),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => startAddingTransactions(context),
+      ),
+    );
   }
 }
