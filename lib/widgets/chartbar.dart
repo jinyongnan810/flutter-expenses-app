08c1333157210ec48b96c826886eba67efb8200c
Yuunan Kin <yuunan.kin@gmail.com>
2021-07-04 08:45:06 +0900

    make bar chart with stack & set alignment

--- lib/widgets/chartbar.dart
+++ lib/widgets/chartbar.dart
@@ -9,7 +9,26 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('\$ ${oneDayAmount.amount.toStringAsFixed(2)}'),
+        Text('\$${oneDayAmount.amount.toStringAsFixed(2)}'),
+        Container(
+          width: 10,
+          height: 100,
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.amber,
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: 10,
+                  height: 100 * oneDayAmount.percentage,
+                  color: Theme.of(context).primaryColor,
+                ),
+              )
+            ],
+          ),
+        ),
         Text('${(oneDayAmount.percentage * 100).toStringAsFixed(2)}%'),
         Text(oneDayAmount.day)
       ],
