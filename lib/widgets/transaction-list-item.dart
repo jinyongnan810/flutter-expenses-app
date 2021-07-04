f5c3c0afe97c3faaeb23cb09266abb23e57503ab
Yuunan Kin <yuunan.kin@gmail.com>
2021-07-05 08:20:18 +0900

    use ListTile instead of rows and columns

--- lib/widgets/transaction-list-item.dart
+++ lib/widgets/transaction-list-item.dart
@@ -8,41 +8,27 @@ class TransactionListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-          child: Row(
-        children: [
-          Container(
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      width: 2, color: Theme.of(context).primaryColor)),
-              width: 100,
-              alignment: Alignment.center,
-              child: FittedBox(
-                child: Text(
-                  '\$ ${_tx.amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).primaryColor),
-                ),
-              )),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FittedBox(
-                  child: Text(_tx.title,
-                      style: Theme.of(context).textTheme.headline6)),
-              Text(
-                DateFormat('yyyy-MM-dd hh:mm:ss').format(_tx.date),
-                style: TextStyle(color: Colors.blueGrey),
-              )
-            ],
-          )
-        ],
-      )),
+    return Card(
+      elevation: 5,
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 50,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(
+              child: Text(
+                '\$ ${_tx.amount.toStringAsFixed(1)}',
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+          ),
+        ),
+        title: Text(_tx.title, style: Theme.of(context).textTheme.headline6),
+        subtitle: Text(
+          DateFormat('yyyy-MM-dd hh:mm:ss').format(_tx.date),
+          style: TextStyle(color: Colors.blueGrey),
+        ),
+      ),
     );
   }
 }
